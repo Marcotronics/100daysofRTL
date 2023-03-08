45e6c7c878b0b20b821bce2bd047af9379cc2049
“Marco <marco.mamani.a@uni.pe>
2023-03-08 14:21:58 -0500

    day 08 minor changes

--- 008_SHIFT_REGISTER/sr.sv
+++ 008_SHIFT_REGISTER/sr.sv
@@ -5,17 +5,17 @@ module sr(
     output logic [7:0] q
 );
 
-logic [6:0] sq;
+logic [7:0] sq;
 
-assign sq = q[6:0];
+assign q = sq;
 
 always_ff @(posedge clk) begin
     if (rst) begin
-        q <=8'h00;
+        sq <=8'h00;
     end 
     else begin
-        q[0] <= a;
-        q[7:1] <= sq;
+        sq[0] <= a;
+        sq[7:1] <= sq[6:0];
     end
 end
 
